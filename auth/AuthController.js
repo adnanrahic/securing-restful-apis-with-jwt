ea8804cb377583d8ab2cd26809a227819b98c8ce
adnanrahic <ado.raha198@gmail.com>
2017-08-11 18:52:09 +0200

    misc cleanup

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -38,9 +38,7 @@ router.post('/login', function(req, res) {
 });
 
 router.get('/logout', function(req, res) {
-
-    res.status(200).send({ auth: false, token: null });
-
+  res.status(200).send({ auth: false, token: null });
 });
 
 router.post('/register', function(req, res) {
